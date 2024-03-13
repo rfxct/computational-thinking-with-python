d7e026576031141bb032c3975f750ce67e430171
rfxct <xbrcvar@gmail.com>
2024-03-12 21:47:59 -0300

    adicionar elif para não testar desnecessáriamente

--- 2024-03-12-condicionais/lista-exercicio/exercicio-7.py
+++ 2024-03-12-condicionais/lista-exercicio/exercicio-7.py
@@ -1,12 +1,14 @@
 total_lados = int(input('Diga quantos lados tem o polígono: '))
 medida_lado = int(input('Diga a medida do lado do polígono: '))
 
-if (total_lados == 3):
+if total_lados == 3:
     print('A figura é um triângulo')
-if total_lados == 4:
+elif total_lados == 4:
     print('A figura é um quadrado')
-if total_lados == 5:
+elif total_lados == 5:
     print('A figura é um pentágono')
+else:
+    print('Polígono não identificado')
 
 perimetro = total_lados * medida_lado
 print(f'O perímetro do objeto é {perimetro}')
