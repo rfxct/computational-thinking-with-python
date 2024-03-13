7614c2b31e6b3d713e58b15294c69afa6585b40f
rfxct <xbrcvar@gmail.com>
2024-03-12 21:43:58 -0300

    ajsutar ordem

--- 2024-03-12-condicionais/lista-exercicio/exercicio-6.py
+++ 2024-03-12-condicionais/lista-exercicio/exercicio-6.py
@@ -2,8 +2,8 @@ altura = float(input('Diga sua altura (m): '))
 sexo = int(input('Diga seu sexo (1:Mulher / 2:Homem): '))
 
 if (sexo == 1):
-    peso_ideal = 72.7 * altura - 58
-else:
     peso_ideal = 62.1 * altura - 44.7
+else:
+    peso_ideal = 72.7 * altura - 58
 
 print(f'O seu peso ideal é de {peso_ideal:.2f}')
