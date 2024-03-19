93906a4350150bca9bc3711e82244be90b71c165
Marcos <71356853+rfxct@users.noreply.github.com>
2024-03-19 19:41:11 -0300

    resoluçâo final

--- 2024-03-12-condicionais/lista-exercicio/desafio.py
+++ 2024-03-12-condicionais/lista-exercicio/desafio.py
@@ -1,16 +1,30 @@
-'''
-Velocidade entre 100 e 120 multa de V * 20%
-Velocidade entre 120 e 150 multa de 120 * 20% + V * 30%
-Velocidade acima de 150 multa de 120 * 20% + 150 * 30% + V*40%
-'''
+# ate 100 sem multa
+# ate 120 -> 20% * V
+# ate 150 -> 120 * 20% + 30% * v
+# mais de 150 -> 120 * 20% + 150 * 30% + 40% * v
 
-velocidade = int(input('Diga a velocidade utilizada: '))
+while True:
+    velocidade = int(input('Digite a velocidade: '))
+    multa = 0
 
-multa = 0
+    # Jeito 1
+    if (velocidade > 100):
+        multa += .2 * (120 if velocidade >= 120 else velocidade)
 
-if velocidade >= 100 and velocidade <= 120:
-    print()
-elif velocidade > 120 and velocidade <= 150:
-    print()
-elif velocidade > 150:
-    print()
+    if (velocidade > 120):
+        multa += .3 * (150 if velocidade >= 150 else velocidade)
+
+    if (velocidade > 150):
+        multa += .4 * velocidade
+    print(f'[Jeito 1] O valor da multa é de R${multa:.2f}')
+        
+    # Jeito 2
+    multa = 0
+    
+    if velocidade > 100 and velocidade <= 120:
+        multa = velocidade * .2
+    elif velocidade > 120 and velocidade <= 150:
+        multa = 120 * .2 + velocidade * .3
+    elif velocidade > 150:
+        multa = 120 * .2 + 150 * .3 + velocidade * .4
+    print(f'[Jeito 2] O valor da multa é de R${multa:.2f}')
