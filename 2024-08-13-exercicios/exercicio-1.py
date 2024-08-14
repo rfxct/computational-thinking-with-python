f3fd94067dc6c706f1d7fed7902482ee293ad214
Marcos <71356853+rfxct@users.noreply.github.com>
2024-08-13 21:43:48 -0300

    adicionar função de primos

--- 2024-08-13-exercicios/exercicio-1.py
+++ 2024-08-13-exercicios/exercicio-1.py
@@ -24,4 +24,11 @@ def numero_primo(num: int):
     for i in range(2, num):
         if num % i == 0:
             return False
-    return True
+    return False if num == 1 else True
+
+'''
+Escreva uma função que recebe um número, e devolve ua lista de todos os números primos até esse limite.
+Se primos(100), retornaremos a lista de todos os primos menores que 100
+'''
+def primos(num: int):
+   return [i for i in range(2, num) if numero_primo(i)]
